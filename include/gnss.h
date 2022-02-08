b0281ae6c8df28a5c97d835f953c45d5b4273039
Sonia Wong <sonia.wy.wong@gmail.com>
2022-02-08 02:18:38 -0800

    Create GNSS header

---
+++ include/gnss.h
@@ -0,0 +1,16 @@
+#ifndef GNSS_H
+#define GNSS_H
+
+namespace GNSS {
+    struct Coordinates {
+        float latitude;
+        float longitude;
+        float altitude;
+    };
+
+    void Setup() {
+        
+    }
+}
+
+#endif
