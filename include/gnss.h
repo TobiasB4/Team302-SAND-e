3bc230d3ffb43fe41711f7ea5c403885b1e21089
Sonia Wong <sonia.wy.wong@gmail.com>
2022-02-08 19:35:42 -0800

    Update includes

--- include/gnss.h
+++ include/gnss.h
@@ -1,16 +1,19 @@
 #ifndef GNSS_H
 #define GNSS_H
 
+#include <stdint.h>
+#include <stddef.h>
+
 namespace GNSS {
     struct Coordinates {
-        float latitude;
-        float longitude;
-        float altitude;
+        double latitude;
+        double longitude;
+        double altitude;
     };
 
-    void Setup() {
-        
-    }
+    void Setup();
+
+    void Message(uint8_t packet_id, uint8_t* payload, size_t payload_length);
 }
 
 #endif
