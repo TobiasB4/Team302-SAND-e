6e45fbfd78ec21f7477debc8a30ed32ee0d90417
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2022-02-21 15:04:11 -0800

    Just changed the main function to include arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "../include/robot.hpp"
 
-int main(){
+int main(int argc, char *argv[]){
     list<long double> resX,resY;
     std::tie(resX,resY)= Map::ExtractFile("T:\\Project SAND-e\\include\\coordinates.txt");
     std::cout.precision(20);
