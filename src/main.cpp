d76852f2d5782b187b254539abf1be62abaa6005
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2022-02-22 13:44:42 -0800

    Changed coordinates absolute path for testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]){
     list<long double> resX,resY;
-    std::tie(resX,resY)= Map::ExtractFile("T:\\Project SAND-e\\include\\coordinates.txt");
+    std::tie(resX,resY)= Map::ExtractFile("C:\\Users\\TobiB\\Documents\\Project_SAND-e\\Software\\include\\coordinates.txt");
     std::cout.precision(20);
     for (long double x: resX){
         std::cout << x << std::endl;
