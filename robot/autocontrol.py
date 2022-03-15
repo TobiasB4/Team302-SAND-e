41c614f58b7c0cc023e68d08d2172ea338ab58c8
fruitbyte <sonia.wy.wong@gmail.com>
2022-03-15 16:44:05 -0700

    Added equations in autocontrol.py

--- robot/autocontrol.py
+++ robot/autocontrol.py
@@ -1,19 +1,29 @@
 # Contains functions for calculations required in automatic control
 
 import math
+from numpy import degrees, deg2rad
 
-# Calculate angle between current heading and intended heading
-def calcAngle(n):
-    return 0
-
+# TODO: verify functionality
 # Calculate desired heading
-def calcDesired(n):
-    return 0
+# Inputs: current lat & long, intended lat & long
+# Outputs: Bearing angle in degrees that needs to be moved
+def calcBearing(currentLat, currentLong, goalLat, goalLong):    
+    calc_x = math.cos(goalLat) * math.sin(goalLong - currentLong)
+    calc_y = math.cos(currentLat) * math.sin(goalLat) - (math.sin(currentLat) * math.cos(goalLat) * math.cos(goalLong - currentLong))
+    bearing = degrees(math.atan2(calc_x, calc_y))
+    return bearing
 
-# Convert degrees to radians, v.v.
-def deg2rad(n):
-    return n * math.pi / 180
-
-def rad2deg(n):
-    return n * 180 / math.pi
-
+# TODO: verify functionality
+# Calculate great-circle distance in meters between current point and goal using the Haversine formula
+# Inputs: current lat & long, intended lat & long
+# Outputs: distance (m)
+def distBetween(currentLat, currentLong, goalLat, goalLong):
+    lat1 = deg2rad(currentLat)
+    long1 = deg2rad(currentLong)
+    lat2 = deg2rad(goalLat)
+    long2 = deg2rad(goalLong)
+    diffLat = lat2 - lat1
+    diffLong = long2 - long1 
+    distance = 2*math.asin(math.sqrt((math.sin(diffLat/2)**2)+((1-(math.sin(diffLat/2)**2)-(math.sin((lat1+lat2)/2)**2))*(math.sin(diffLong/2)**2))))
+    distance *= 6378137 # Earth's equatorial radius in m from https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+    return distance
--- robot/main.py
+++ robot/main.py
@@ -2,3 +2,9 @@ import setup, asyncio, websockets, serial
 import autocontrol as ac
 from ublox_gps import UbloxGps
 
+# Control loop
+# Every 1 Hz:
+# - set orientation (to face intended GPS heading) (within 5 degrees is ok, pass)
+# - check distance between current position and intended position and set rate accoringly
+# - drive for 1 second blind
+
