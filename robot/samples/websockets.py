f17c7094d7237943967243f20c57ad79334b6bed
fruitbyte <sonia.wy.wong@gmail.com>
2022-03-28 04:43:52 -0700

    Websockets example updated to work with server

--- robot/samples/websockets.py
+++ robot/samples/websockets.py
@@ -1,16 +1,21 @@
 # https://pypi.org/project/websockets/
 # https://realpython.com/async-io-python/
 
+import json
 import asyncio
 import websockets
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
 async def main():
-    async with serve(echo, "localhost", 8765):
-        await asyncio.Future()
+    async with websockets.connect("ws://147.182.255.197:8000") as websocket:
+        event = {
+            "type":"kinetics_bot",
+            "velocity": 125,
+            "acceleration": 10,
+            "key": "robot",
+        }
+        await websocket.send(json.dumps(event))
+        message = await websocket.recv()
+        print(message)
 
 asyncio.run(main())
 
