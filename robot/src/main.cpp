1728a2e0dcf8250d61429c08f536e872236f0e57
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2022-05-31 15:05:15 -0700

    Cleaned up main file

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -1,14 +1,9 @@
 #include "../include/robot.hpp"
 
 int main(int argc, char *argv[]){
-    //Map::XY_Pair gps = Map::XY_Pair(Map::ExtractFile("T:\\Project SAND-e\\Team302-SAND-e\\robot\\include\\coordinates.txt"));
     vector<Map::Coordinates> gps = Map::ExtractFile("C:\\Users\\TobiB\\Documents\\Project_SAND-e\\Software\\robot\\include\\coordinates.txt");
-    //Map::XY_Pair gps = Map::XY_Pair(Map::ExtractFile(argv[1]));
-
     std::ofstream file;
-    //file.open("T:\\Project SAND-e\\Team302-SAND-e\\robot\\include\\newCoordinates.txt");
     file.open("C:\\Users\\TobiB\\Documents\\Project_SAND-e\\Software\\robot\\include\\newCoordinates.txt");
-    //Autonomous::PathFinding::CreatePoints(gps.gps[0],gps.gps[1],gps.gps[gps.gps.size()-1],2.5L,file);
     Autonomous::PathFinding::DrawMap(gps,2.5L,file);
     file.close();
     return 0;
